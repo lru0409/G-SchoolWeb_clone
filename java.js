a92b0f5f61c0da8ddc16c6171b10dc1f645bac2a
lru0409 <74223246+lru0409@users.noreply.github.com>
2020-12-09 10:11:59 +0900

    12월 9일 웹사이트 웹에 올리기

---
+++ java.js
@@ -0,0 +1,11 @@
+
+window.onload = function() {
+
+    document.getElementByclassName('christmasedition').addEventListener('click', function() {
+
+    document.getElementByclassName('christmasedition').style.background = 'red';
+    
+
+    
+    });
+}
